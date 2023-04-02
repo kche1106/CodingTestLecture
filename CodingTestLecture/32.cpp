7b87527213a58d531d7c8cfd2ceb981a37d20ceb
kche1106 <kche011106@gmail.com>
2023-04-02 18:21:41 +0900

    32

---
+++ CodingTestLecture/32.cpp
@@ -0,0 +1,33 @@
+//
+//  32.cpp
+//  CodingTestLecture
+//
+//  Created by 김채은 on 2023/04/02.
+//
+
+#include <iostream>
+using namespace std;
+
+//선택 정렬
+int main() {
+    int a[100], n, idx;
+    cin >> n;
+    
+    for(int i = 0; i < n; i++) {
+        cin >> a[i];
+    }
+    
+    for(int i = 0; i < n-1; i++) {
+        idx = i;
+        for(int j = i + 1; j < n; j++) {
+            if(a[j] < a[idx]) idx = j;
+        }
+        int tmp = a[i];
+        a[i] = a[idx];
+        a[idx] = tmp;
+    }
+    
+    for(int i = 0; i < n; i++) {
+        cout << a[i] << " ";
+    }
+}
