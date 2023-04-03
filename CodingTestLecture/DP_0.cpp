f3bcd626a65d86e86b3171c775e72deac3ca6655
kche1106 <kche011106@gmail.com>
2023-04-03 11:25:05 +0900

    DP_0

---
+++ CodingTestLecture/DP_0.cpp
@@ -0,0 +1,16 @@
+//
+//  DP_0.cpp
+//  CodingTestLecture
+//
+//  Created by 김채은 on 2023/04/03.
+//
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    ios_base::sync_with_stdio(false); //동기화 해제, c++ 표준 스트림만 사용(c++의 독립 버퍼 사용)
+    //ios::sync_with_stdio(false);
+    //iostream::sync_with_stdio(false);
+    cin.tie(NULL);  //untie
+}
