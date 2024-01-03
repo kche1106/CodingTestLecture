51640bd7af6f80538c2af2d7ac029b17ef4f0999
kche1106 <kche011106@gmail.com>
2024-01-03 21:12:37 +0900

    61

--- CodingTestLecture/61.cpp
+++ CodingTestLecture/61.cpp
@@ -8,17 +8,26 @@
 #include <iostream>
 using namespace std;
 
-int arr[11];
+int arr[11], trac[11];
 int n, m, res;
 
 void DFS(int i, int sum) {
     if(i == n) {
-        if(sum == m) res++;
+        if(sum == m) {
+            res++;
+            for(int j = 0; j < n; j++) {
+                cout << trac[j] << " ";
+            }
+            cout << endl;
+        }
     }
     
     else {
+        trac[i] = arr[i];
         DFS(i+1, sum + arr[i]);
+        trac[i] = -arr[i];
         DFS(i+1, sum - arr[i]);
+        trac[i] = 0;
         DFS(i+1, sum);
     }
 }
