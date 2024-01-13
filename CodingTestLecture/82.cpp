a2e2eb42c9193d0b88d8c97c73f3d703af7a9af3
kche1106 <kche011106@gmail.com>
2024-01-13 12:21:44 +0900

    82

--- CodingTestLecture/82.cpp
+++ CodingTestLecture/82.cpp
@@ -5,4 +5,39 @@
 //  Created by 김채은 on 2024/01/11.
 //
 
-#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+int num[16], isvisit[16], result[16];
+int n, r, cnt;
+
+void DFS(int l) {
+    if(l == r) {
+        for(int i = 0; i < l; i++) {
+            cout << result[i] << " ";
+        }
+        cout << endl;
+        cnt++;
+    }
+    else {
+        for(int i = 0; i < n; i++) {
+            if(isvisit[i] == 0) {
+                result[l] = num[i];
+                isvisit[i] = 1;
+                DFS(l+1);
+                isvisit[i] = 0;
+            }
+        }
+    }
+}
+
+int main() {
+    cin >> n >> r;
+    
+    for(int i = 0; i < n; i++) {
+        cin >> num[i];
+    }
+    
+    DFS(0);
+    cout << cnt << endl; 
+}
