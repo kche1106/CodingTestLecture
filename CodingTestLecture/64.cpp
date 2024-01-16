4b6c0de502c0400d47509eb23c4f06e3febf38dd
kche1106 <kche011106@gmail.com>
2024-01-16 18:36:14 +0900

    64

--- CodingTestLecture/64.cpp
+++ CodingTestLecture/64.cpp
@@ -5,4 +5,38 @@
 //  Created by 김채은 on 2024/01/16.
 //
 
-#include <stdio.h>
+#include <iostream>
+using namespace std;
+
+vector<int> graph[20];
+int visited[20];
+int res;
+int n, m;
+
+void dfs(int x) {
+    
+    if(x == n) res++;
+    
+    for(int i = 0; i < graph[x].size(); i++) {
+        if(!visited[graph[x][i]]) {
+            visited[graph[x][i]] = 1;
+            dfs(graph[x][i]);
+            visited[graph[x][i]] = 0;
+        }
+    }
+}
+
+int main() {
+    cin >> n >> m;
+    
+    for(int i = 0; i < m; i++) {
+        int a, b;
+        cin >> a >> b;
+        graph[a].push_back(b);
+    }
+    
+    visited[1] = 1;
+    dfs(1);
+    
+    cout << res << endl;
+}
