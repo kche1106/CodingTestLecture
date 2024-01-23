a6fbc21669d2395a2fb5f1ea69edc6c38475dd04
kche1106 <kche011106@gmail.com>
2024-01-23 20:27:29 +0900

    72

--- CodingTestLecture/72.cpp
+++ CodingTestLecture/72.cpp
@@ -5,4 +5,28 @@
 //  Created by 김채은 on 2024/01/23.
 //
 
-#include <stdio.h>
+#include <iostream>
+#include <queue>
+using namespace std;
+
+int main() {
+    int n, k;
+    cin >> n >> k;
+    
+    deque<int> dq;
+    for(int i = 1; i <= n; i++) {
+        dq.push_back(i);
+    }
+    
+    while(true) {
+        if(dq.size() == 1) break;
+        
+        for(int i = 1; i < k; i++) {
+            dq.push_back(dq.front());
+            dq.pop_front();
+        }
+        dq.pop_front();
+    }
+    
+    cout << dq.front() << endl;
+}
